ac8744c3eeaf08520343de24d42ee6726c4d949b
antonioaquinob <antonioaquinob@gmail.com>
2025-11-16 21:44:59 +0800

    added implementation of interface from .core in .Api /Repository (BookRepository)

---
+++ LibraryManagementSystem.API/Migrations/20251116132217_UpdateBookEntity.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryManagementSystem.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBookEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PublishedMonth",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "PublishedYear",
+                table: "Books");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PublishDate",
+                table: "Books",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "PublishDate",
+                table: "Books");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PublishedMonth",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PublishedYear",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
---
+++ LibraryManagementSystem.API/Repositories/BookRepository.cs
@@ -0,0 +1,52 @@
+﻿using LibraryManagementSystem.Core.Interfaces;
+using LibraryManagementSystem.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using LibraryManagementSystem.API.Data;
+using System.Runtime.InteropServices;
+namespace LibraryManagementSystem.API.Repositories
+{
+    public class BookRepository: IBookRepository
+    {
+        private readonly LibraryDbContext _context;
+        public BookRepository(LibraryDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Book>> GetAllAsync()
+        {
+            return await _context.Books.ToListAsync();
+        }
+        public async Task<Book?> GetByIdAsync(int bookId)
+        {
+            return await _context.Books.FindAsync(bookId);
+        }
+        public async Task<Book> AddAsync(Book book)
+        {
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+            return book;
+        }
+        public async Task<Book> UpdateAsync(Book book)
+        {
+            _context.Books.Update(book);
+            await _context.SaveChangesAsync();
+            return book;
+        }
+        public async Task<bool> DeleteAsync(Book book)
+        {
+            var findBook = await _context.Books.FindAsync(book.BookId);
+            if (findBook != null)
+                return false;
+
+            _context.Books.Remove(book);
+            await _context.SaveChangesAsync();
+            return true;
+
+        }
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
